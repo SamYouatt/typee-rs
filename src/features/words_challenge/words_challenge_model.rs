8ec51ce565582baa9f8e2975ae0b636c7bbe73bf
SamYouatt <sdyouatt@gmail.com>
2024-09-11 21:44:16 +0100

    red: test should track current progress and move forwards regardless of correct or incorrect input - it breaks typing flow for me otherwise and means you can go back and fix mistakes

--- src/features/words_challenge/words_challenge_model.rs
+++ src/features/words_challenge/words_challenge_model.rs
@@ -1,11 +1,55 @@
+use crossterm::event::KeyCode;
+
 pub struct WordsChallengeModel {
     text: String,
+
+    current_pos: usize,
 }
 
 impl WordsChallengeModel {
     pub fn generate(length: usize) -> Self {
         let text = "bongle ".repeat(length);
 
-        Self { text }
+        Self {
+            text,
+            current_pos: 0,
+        }
+    }
+
+    pub fn handle_input(self, keycode: KeyCode) -> Self {
+        match keycode {
+            KeyCode::Char(char) => todo!(),
+            _ => self
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn model_with_text(text: impl ToString) -> WordsChallengeModel {
+        WordsChallengeModel {
+            text: text.to_string(),
+            current_pos: 0,
+        }
+    }
+
+    #[test]
+    fn correct_input_advances_challenge() {
+        let model = model_with_text("test");
+
+        let result = model.handle_input(KeyCode::Char('t'));
+
+        assert_eq!(result.current_pos, 1);
+    }
+
+    #[test]
+    fn incorrect_input_advances_challenge() {
+        let model = model_with_text("test");
+
+        let result = model.handle_input(KeyCode::Char('x'));
+
+        assert_eq!(result.current_pos, 1);
     }
 }
