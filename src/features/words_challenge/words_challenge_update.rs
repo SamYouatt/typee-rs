c033a9405663da898a94a7222b7e49938d5d9772
SamYouatt <sdyouatt@gmail.com>
2024-10-02 20:33:30 +0100

    green: unnest by extractint the challenge first

--- src/features/words_challenge/words_challenge_update.rs
+++ src/features/words_challenge/words_challenge_update.rs
@@ -5,26 +5,28 @@ pub fn words_challenge_update(
     model: AppModel,
     message: Message,
 ) -> Result<(AppModel, Option<Message>)> {
-    match model.app_state {
-        AppPage::WordsChallenge(challenge) => match message {
-            Message::ChallengeLetterInput(keycode) => {
-                let new_model = AppModel {
-                    app_state: AppPage::WordsChallenge(challenge.handle_challenge_input(keycode)),
-                    ..model
-                };
-                Ok((new_model, None))
-            }
-            _ => Ok((
-                AppModel {
-                    app_state: AppPage::WordsChallenge(challenge),
-                    ..model
-                },
-                None,
-            )),
-        },
+    let challenge = match model.app_state {
+        AppPage::WordsChallenge(challenge) => challenge,
         _ => panic!(
             "words challenge cannot use non words challenge models, found: {:?}",
             model.app_state
         ),
+    };
+
+    match message {
+        Message::ChallengeLetterInput(keycode) => {
+            let new_model = AppModel {
+                app_state: AppPage::WordsChallenge(challenge.handle_challenge_input(keycode)),
+                ..model
+            };
+            Ok((new_model, None))
+        }
+        _ => Ok((
+            AppModel {
+                app_state: AppPage::WordsChallenge(challenge),
+                ..model
+            },
+            None,
+        )),
     }
 }
